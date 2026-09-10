cd67c470be0466f270f9aa323c80404daa6271a5
Rich-2024 <richardogwal97@gmail.com>
2026-09-10 10:10:34 +0300

    TV: professional coach bus SVG with bounce animation, motion lines, gold tint

--- app.py
+++ app.py
@@ -651,6 +651,10 @@ def bus_display():
 def bus_conductor():
     return send_from_directory('.', 'bus-conductor.html')
 
+@app.route('/bus.svg')
+def bus_svg():
+    return send_from_directory('.', 'bus.svg')
+
 @app.route('/admin')
 def admin_decoy():
     return redirect('/')
